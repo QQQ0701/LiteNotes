e5b7f539c58b51580ec153b285485d597eda92af
Calvin <jianwei2526056@gmail.com>
2026-04-06 10:07:32 +0800

    feat(infra): 實作相關 IValueConverter - FileSizeConverter 轉換可讀格式（KB/MB） - BoolToAttachmentIconConverter 附件類型圖示切換 -CountToVisibilityConverter 附件清單顯示狀態

---
+++ StockEvernote/Converters/FileSizeConverter.cs
@@ -0,0 +1,20 @@
+﻿using StockEvernote.Services;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace StockEvernote.Converters;
+
+public class FileSizeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is long bytes)
+        {
+            return AzureBlobService.FormatFileSize(bytes);
+        }
+        return "0 B";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
